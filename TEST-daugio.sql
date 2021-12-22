383278b6d444478b8e5b07c8eb95b9167a540b41
ChuTienDat <91305197+Chutiendat26092003@users.noreply.github.com>
2021-12-22 08:29:24 +0700

    Add files via upload

---
+++ TEST-daugio.sql
@@ -0,0 +1,24 @@
+﻿
+
+USE TESTDB
+GO
+
+CREATE TABLE Student(
+    NameStudent VARCHAR(50) NOT NULL,
+	DateOfBirth VARCHAR(10) NOT NULL,
+	Class VARCHAR(10)
+)
+INSERT INTO Student VALUES (N'Nguyễn Văn A', N'1/1/2002', N'T2000A')
+INSERT INTO Student VALUES (N'Nguyễn Văn B', N'1/1/2001', N'T2000A')
+
+SELECT * FROM Student
+
+ALTER TABLE Student
+    ALTER COLUMN Class varchar(20)
+
+ALTER TABLE Student
+    ADD CONSTRAINT PK_M PRIMARY KEY (NameStudent)
+
+ALTER TABLE Student
+    DROP CONSTRAINT PK_M
+ 
